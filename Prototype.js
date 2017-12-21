9ec90b6775852b1afa542e50ba2ee8da7ddfd9dd
August-Z <402279076@qq.com>
2017-12-22 00:31:11 +0800

    原型模式

--- Prototype.js
+++ Prototype.js
@@ -1,11 +1,11 @@
 //原型模式
 
-let myFriend = {
+const myFriend = {
     name: "Allen",
-    playDota() {
+    playGame() {
         console.log("First blood  ～～");
     },
-    learnJavaScript(){
+    learnJavaScript() {
         console.log("让我们每周掌握一种设计模式！");
     }
 };
@@ -13,5 +13,12 @@ let myFriend = {
 const yourFriend = Object.create(myFriend);
 
 console.log(yourFriend.name);   // Allen
+yourFriend.playGame();  // 输出 First blood  ～～
 
-yourFriend.playDota();  // 输出 First blood  ～～
+//如果你的朋友 jay 现在想玩别的游戏，比如 LOL，我们可以修改一下
+myFriend.name = "jay";
+myFriend.playGame = game => console.log(myFriend.name + " 正在玩 " + game + " ！");
+
+yourFriend.playGame("lol"); //输出 jay 正在玩 lol ！
+
+
