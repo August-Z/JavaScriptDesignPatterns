b03ce0ed68c6ae644288c0cda2be510b334ebb96
August-Z <402279076@qq.com>
2017-12-22 00:32:40 +0800

    原型模式

--- Prototype.js
+++ Prototype.js
@@ -17,7 +17,7 @@ yourFriend.playGame();  // 输出 First blood  ～～
 
 //如果你的朋友 jay 现在想玩别的游戏，比如 LOL，我们可以修改一下
 myFriend.name = "jay";
-myFriend.playGame = game => console.log(myFriend.name + " 正在玩 " + game + " ！");
+myFriend.playGame = game => console.log(`${myFriend.name} 正在玩 ${game} ！`);
 
 yourFriend.playGame("lol"); //输出 jay 正在玩 lol ！
 
